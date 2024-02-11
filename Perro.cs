0a567b9c2ea0ba9aec0f9dcd49f9118dfc500a40
Osmary <Osmary@DESKTOP-8DOM8BM>
2024-02-11 09:50:07 -0500

    Agregar archivos de proyecto.

---
+++ Perro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    public class Perro:Animal
+    {
+
+        public override void HacerSonido()
+        {
+            Console.WriteLine("El perro ladra: !Guau, guau¡");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Animal;
+
+Console.WriteLine("Animal");
+Console.WriteLine();
+
+Perro  perro = new Perro();
+
+Gato gato = new Gato();
+
+Console.WriteLine("Mi perro:");
+perro.HacerSonido();
+Console.WriteLine();
+Console.WriteLine("Mi gato:");
+gato.HacerSonido();
+
